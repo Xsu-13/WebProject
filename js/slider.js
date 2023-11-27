e37caac7e8a91f4fa392c495840ba4e6de5d0f35
Kseniia Kosonogova <k.kosonogova@xlab-game.com>
2023-11-27 03:38:51 +0300

    slider bugs

--- js/slider.js
+++ js/slider.js
@@ -1,17 +1,35 @@
 $(document).ready(function(){
     $('.slider').slick({
-        slidesToShow: 3,
-        dots:true
+        slidesToShow: 5,
+        centerMode: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        dots:false,
+        prevArrow: false,
+        nextArrow: false
     });
-    function changeSlider(x)
-    {
-        if (x.matches) {
-            $('.slider').slick('slickSetOption', 'slidesToShow', 3);
-          } else {
-            $('.slider').slick('slickSetOption', 'slidesToShow', 1);
-          }
-    }
-    let x = window.matchMedia("(min-width: 700px)");
-    changeSlider(x);
-    x.addEventListener('change', changeSlider, x);
+    $('.comment-slider').slick({
+        adaptiveHeight:true,
+        centerMode:false,
+        slidesToShow: 1,
+        autoplay: false,
+        dots:false,
+        draggable: false,
+        fade: true,
+        prevArrow: $('comment-arrow-prev'),
+        nextArrow: $('comment-arrow-next')
+    });
+
+    $('.comment-card-wrapper').on('init reInit afterChange', function(event, slick, currentSlide, nextSlide){
+        var i = (currentSlide ? currentSlide : 0) + 1;
+        $(this).find('.current-num').html("0" + i);
+    });
+
+    $('.comment-arrow-prev').on('click', function() {
+        $('.comment-slider').slick('slickPrev');
+    });
+    $('.comment-arrow-next').on('click', function() {
+        $('.comment-slider').slick('slickNext');
+    });
+    
 })
