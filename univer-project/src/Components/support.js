109a209c1333cf0f0a46190335eefbb6f17aea2f
Kseniia Kosonogova <k.kosonogova@xlab-game.com>
2023-12-14 14:03:48 +0300

    try react 1.0

---
+++ univer-project/src/Components/support.js
@@ -0,0 +1,199 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "../Styles/MainStyle.css"
+import "../Styles/support.css"
+
+function Support() {
+  return (
+    <div>
+    <div class="container support-element">
+        <div class="row">
+            <div class="col-md-12 support-title">
+                <h2 class="support-title black-title">Поддержка <br/> от Drupal-coder</h2>
+            </div>
+        </div>
+        <div class="row flex-row support-row">
+            <div class="col-md-3 col-sm-6 col-xs-12 support-col">
+                <div class="support-block">
+                    <div class="support-block-wrapper">
+                        <div class="support-block-num">
+                            01.
+                        </div>
+                        <div class="support-block-title">
+                            Постановка задачи по Email
+                        </div>
+                        <div class="support-text">
+                            Удобная и привычная модель постановки задач, при которой задачи фиксируются и никогда не теряются.
+                        </div>
+                        <div class="support-icon">
+                            <img src="images/support-1.svg" alt="иконка соответсвуюшая блоку" class="img-responsive"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 support-col">
+                <div class="support-block">
+                    <div class="support-block-wrapper">
+                        <div class="support-block-num">
+                            02.
+                        </div>
+                        <div class="support-block-title">
+                            Система Helpdesk – отчетность, прозрачность
+                        </div>
+                        <div class="support-text">
+                            Возможность посмотреть все заявки в работе <br/>
+                            и отработанные часы <br/>
+                            в личном кабинете через браузер.
+                        </div>
+                        <div class="support-icon">
+                            <img src="images/pie-chart.svg" alt="иконка соответсвуюшая блоку" class="img-responsive"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 support-col">
+                <div class="support-block">
+                    <div class="support-block-wrapper">
+                        <div class="support-block-num">
+                            03.
+                        </div>
+                        <div class="support-block-title">
+                            Расширенная техническая <br/> поддержка
+                        </div>
+                        <div class="support-text">
+                            Возможность организации расширенной техподдержки с 6:00 <br/>до 22:00 без выходных.
+                        </div>
+                        <div class="support-icon">
+                            <img src="images/settings.svg" alt="иконка соответсвуюшая блоку" class="img-responsive"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 support-col">
+                <div class="support-block">
+                    <div class="support-block-wrapper">
+                        <div class="support-block-num">
+                            04.
+                        </div>
+                        <div class="support-block-title">
+                            Персональный менеджер проекта
+                        </div>
+                        <div class="support-text">
+                            Ваш менеджер проекта  всегда в курсе текущего состояния проекта <br/>и в любой момент готов ответить на любые вопросы.
+                        </div>
+                        <div class="support-icon">
+                            <img src="images/group1.svg" alt="иконка соответсвуюшая блоку" class="img-responsive"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row flex-row support-row">
+            <div class="col-md-3 col-sm-6 col-xs-12 support-col">
+                <div class="support-block">
+                    <div class="support-block-wrapper">
+                        <div class="support-block-num">
+                            05.
+                        </div>
+                        <div class="support-block-title">
+                            Удобные способы оплаты
+                        </div>
+                        <div class="support-text">
+                            Безналичный расчет по договору или электронные деньги: WebMoney, Яндекс.Деньги, Paypal.
+                        </div>
+                        <div class="support-icon">
+                            <img src="images/support-5.svg" alt="иконка соответсвуюшая блоку" class="img-responsive"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 support-col">
+                <div class="support-block">
+                    <div class="support-block-wrapper">
+                        <div class="support-block-num">
+                            06.
+                        </div>
+                        <div class="support-block-title">
+                            Работаем с SLA <br/>и NDA
+                        </div>
+                        <div class="support-text">
+                            </div>Работа в рамках соглашений <br/> о конфиденциальности<br/>и об уровне качетсва работ.
+                        </div>
+                        <div class="support-icon">
+                            <img src="images/support-6.svg" alt="иконка соответсвуюшая блоку" class="img-responsive"/>
+                        </div>
+                    </div>
+                </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 support-col">
+                <div class="support-block">
+                    <div class="support-block-wrapper">
+                        <div class="support-block-num">
+                            07.
+                        </div>
+                        <div class="support-block-title">
+                            Штатные<br/>специалисты
+                        </div>
+                        <div class="support-text">
+                            Надежные штатные специалисты, никаких фрилансеров.
+                        </div>
+                        <div class="support-icon">
+                            <img src="images/group.svg" alt="иконка соответсвуюшая блоку" class="img-responsive"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3 col-sm-6 col-xs-12 support-col">
+                <div class="support-block">
+                    <div class="support-block-wrapper">
+                        <div class="support-block-num">
+                            08.
+                        </div>
+                        <div class="support-block-title">
+                            Удобные каналы<br/>связи
+                        </div>
+                        <div class="support-text">
+                            Консультации по телефону, скайпу,<br/>в месенджерах.
+                        </div>
+                        <div class="support-icon">
+                            <img src="images/support-8.svg" alt="иконка соответсвуюшая блоку" class="img-responsive"/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="expertise">
+    <div class="row flex-row">
+        <div class="col-md-6 col-sm-12 col-xs-12 order-1 order-md-0">
+            <div class="expertise-img">
+                <img src="images/laptop.png" alt="иконка соответсвуюшая блоку" class="img-responsive"/>
+            </div>
+        </div>
+        <div class="col-md-6 col-sm-12 col-xs-12 order-0 order-md-1 expertise-info-block">
+            <div class="expertise-title white-title">
+                Экспертиза в Drupal,<br/> опыт 14 лет!
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <div class="expertise-text-block">
+                        <div class="expertise-text white-title">
+                            Только системный подход –<br/>контроль версий,<br/> резервирование<br/>и тестирование!
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <div class="expertise-text-block">
+                        <div class="expertise-text white-title">
+                            Только Drupal сайты,<br/>не берем на поддержку<br/> сайты на других CMS!
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>  
+</div>
+</div>
+  );
+}
+
+export default Support;
+
---
+++ univer-project/src/Components/yearsBlock.js
@@ -0,0 +1,147 @@
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "../Styles/MainStyle.css"
+import "../Styles/13YearsBlock.css"
+
+function YearsBlock() {
+  return (
+    <div class="_13-years-blok">
+    <div class="container">
+        <div class="row col-md-6">
+            <div class="block-title _13-years-name">13 лет совершенствуем <br/> компетенции в Drupal <br/>
+                поддержке!
+            </div>
+            <div class="competencies-13-years-name">
+                Разрабатываем и оптимизируем модули, расширяем <br/> функциональность
+                сайтов, обновляем дизайн
+            </div>
+        </div>
+        <div class="row row-flex competencies-row">
+            <div class="col-sm-3 col-xs-6">
+                <div class="competency">
+                    <div class="competency-wrapper">
+                        <div class="competency-header">
+                            <div class="background-icon">
+                              <img src="images/icon-bg.svg" alt="Задний фон блока 1" class="img-responsive"/></div>
+                            <div class="competency-icon"><img src="images/competency-1.svg" alt="Иконка блока 1"
+                                    class="img-responsive"/></div>
+                        </div>
+                        <div class="competency-body">
+                            Добавление <br/> информации на сайт,<br/> создание новых <br/>
+                            разделов
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 col-xs-6">
+                <div class="competency">
+                    <div class="competency-wrapper">
+                        <div class="competency-header">
+                            <div class="background-icon"><img src="images/icon-bg.svg" alt="Задний фон блока 1"
+                                    class="img-responsive"/></div>
+                            <div class="competency-icon"><img src="images/competency-2.svg" alt="Иконка блока 2"
+                                    class="img-responsive"/></div>
+                        </div>
+                        <div class="competency-body">
+                            Разработка <br/> и оптимизация <br/> модулей сайта
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 col-xs-6">
+                <div class="competency">
+                    <div class="competency-wrapper">
+                        <div class="competency-header">
+                            <div class="background-icon"><img src="images/icon-bg.svg" alt="Задний фон блока 1"
+                                    class="img-responsive"/></div>
+                            <div class="competency-icon"><img src="images/competency-3.svg" alt="Иконка блока 3"
+                                    class="img-responsive"/></div>
+                        </div>
+                        <div class="competency-body">
+                            Интеграция с CRM, <br/> 1C, платежными <br/> системами, любыми
+                            <br/> веб-сервисами
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 col-xs-6">
+                <div class="competency">
+                    <div class="competency-wrapper">
+                        <div class="competency-header">
+                            <div class="background-icon"><img src="images/icon-bg.svg" alt="Задний фон блока 1"
+                                    class="img-responsive"/></div>
+                            <div class="competency-icon"><img src="images/competency-4.svg" alt="Иконка блока 3"
+                                    class="img-responsive"/></div>
+                        </div>
+                        <div class="competency-body">
+                            Любые доработки <br/> функционала <br/> и дизайна
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 col-xs-6">
+                <div class="competency">
+                    <div class="competency-wrapper">
+                        <div class="competency-header">
+                            <div class="background-icon"><img src="images/icon-bg.svg" alt="Задний фон блока 1"
+                                    class="img-responsive"/></div>
+                            <div class="competency-icon"><img src="images/competency-5.svg" alt="Иконка блока 4"
+                                    class="img-responsive"/></div>
+                        </div>
+                        <div class="competency-body">
+                            Аудит и мониторинг <br/> безопасности Drupal <br/> сайтов
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 col-xs-6">
+                <div class="competency">
+                    <div class="competency-wrapper">
+                        <div class="competency-header">
+                            <div class="background-icon"><img src="images/icon-bg.svg" alt="Задний фон блока 1"
+                                    class="img-responsive"/></div>
+                            <div class="competency-icon"><img src="images/competency-6.svg" alt="Иконка блока 5"
+                                    class="img-responsive"/></div>
+                        </div>
+                        <div class="competency-body">
+                            Миграция, импорт <br/> контента и апгрейд <br/> Drupal
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 col-xs-6">
+                <div class="competency">
+                    <div class="competency-wrapper">
+                        <div class="competency-header">
+                            <div class="background-icon"><img src="images/icon-bg.svg" alt="Задний фон блока 1"
+                                    class="img-responsive"/></div>
+                            <div class="competency-icon"><img src="images/competency-7.svg" alt="Иконка блока 6"
+                                    class="img-responsive"/></div>
+                        </div>
+                        <div class="competency-body">
+                            Оптимизация <br/> и ускорение <br/> Drupal-сайтов
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-3 col-xs-6">
+                <div class="competency">
+                    <div class="competency-wrapper">
+                        <div class="competency-header">
+                            <div class="background-icon"><img src="images/icon-bg.svg" alt="Задний фон блока 1"
+                                    class="img-responsive"/></div>
+                            <div class="competency-icon"><img src="images/competency-8.svg" alt="Иконка блока 7"
+                                    class="img-responsive"/></div>
+                        </div>
+                        <div class="competency-body">
+                            Веб-маркетинг, <br/> консультации <br/> и работы по SEO
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>   
+  );
+}
+
+export default YearsBlock;
